327ae70d66388e372f6e4270b646c39af91f40cf
fblaha <blahaf@gmail.com>
2019-07-15 20:57:50 +0300

    support to pass fuctions as workers

--- concurrent/pool.go
+++ concurrent/pool.go
@@ -9,6 +9,9 @@ type Worker interface {
 	Work()
 }
 
+// WorkerFunc does the work
+type WorkerFunc func()
+
 // PoolExecutor distributes works to the pool of goroutines
 type PoolExecutor struct {
 	// tracks completion of submitted work
@@ -16,12 +19,12 @@ type PoolExecutor struct {
 	// tracks pool goroutines which process the incoming work
 	poolWG sync.WaitGroup
 	// incoming work
-	todo chan Worker
+	todo chan WorkerFunc
 }
 
 // NewPoolExecutor constructor
 func NewPoolExecutor(concurrency int) *PoolExecutor {
-	todo := make(chan Worker)
+	todo := make(chan WorkerFunc)
 	executor := PoolExecutor{todo: todo}
 	for i := 0; i < concurrency; i++ {
 		executor.poolWG.Add(1)
@@ -33,13 +36,13 @@ func NewPoolExecutor(concurrency int) *PoolExecutor {
 func (e *PoolExecutor) handleWork() {
 	defer e.poolWG.Done()
 	for w := range e.todo {
-		w.Work()
+		w()
 		e.workWG.Done()
 	}
 }
 
-// Submit submits the work for execution
-func (e *PoolExecutor) Submit(workers ...Worker) {
+// SubmitFunc submits the work for execution
+func (e *PoolExecutor) SubmitFunc(workers ...WorkerFunc) {
 	for _, w := range workers {
 		// ensures that shutdown waits for completion of submitted work
 		e.workWG.Add(1)
@@ -48,12 +51,24 @@ func (e *PoolExecutor) Submit(workers ...Worker) {
 	}
 }
 
+// Submit submits the work for execution
+func (e *PoolExecutor) Submit(workers ...Worker) {
+	for _, w := range workers {
+		e.SubmitFunc(w.Work)
+	}
+}
+
 // ShutdownGracefully waits for completion of the submitted work and terminates worker goroutines
 // and frees allocated resources. The executor can no longer be used after this call.
 func (e *PoolExecutor) ShutdownGracefully() {
 	// waits for completion of submitted work
-	e.workWG.Wait()
+	e.Wait()
 	close(e.todo)
 	// waits for completion of the pool goroutines
 	e.poolWG.Wait()
+}
+
+// Wait waits for completion of the submitted work
+func (e *PoolExecutor) Wait() {
+	e.workWG.Wait()
 }
