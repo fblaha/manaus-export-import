4ed4bf509eede137c57dacb0b9b1a3e1549802c9
fblaha <blahaf@gmail.com>
2019-09-23 22:40:13 +0300

    fmt

--- rest/url.go
+++ rest/url.go
@@ -1,11 +1,11 @@
 package rest
 
 import (
-    "fmt"
-    "io/ioutil"
-    "net/http"
+	"fmt"
+	"io/ioutil"
+	"net/http"
 
-    "github.com/pkg/errors"
+	"github.com/pkg/errors"
 )
 
 type httpClient func(req *http.Request) (*http.Response, error)
@@ -15,20 +15,20 @@ type URLLoader func(url string) ([]byte, error)
 
 // NewURLLoader constructor
 func NewURLLoader(httpClient httpClient) URLLoader {
-    return func(url string) ([]byte, error) {
-        req, err := http.NewRequest("GET", url, nil)
-        if err != nil {
-            return nil, errors.Wrap(err, "unable to create request")
-        }
+	return func(url string) ([]byte, error) {
+		req, err := http.NewRequest("GET", url, nil)
+		if err != nil {
+			return nil, errors.Wrap(err, "unable to create request")
+		}
 
-        res, err := httpClient(req)
-        if err != nil {
-            return nil, errors.Wrap(err, "http request failed")
-        }
-        if res == nil || res.StatusCode != 200 {
-            return nil, fmt.Errorf("unexpected response: %+v", res)
+		res, err := httpClient(req)
+		if err != nil {
+			return nil, errors.Wrap(err, "http request failed")
+		}
+		if res == nil || res.StatusCode != 200 {
+			return nil, fmt.Errorf("unexpected response: %+v", res)
 
-        }
-        return ioutil.ReadAll(res.Body)
-    }
+		}
+		return ioutil.ReadAll(res.Body)
+	}
 }
--- rest/url_test.go
+++ rest/url_test.go
@@ -1,42 +1,42 @@
 package rest
 
 import (
-    "bytes"
-    "fmt"
-    "io/ioutil"
-    "net/http"
-    "testing"
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"testing"
 
-    "github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/require"
 )
 
 func mockClient(mockStatusCode int, mockBody string, mockErr error) httpClient {
-    return func(req *http.Request) (*http.Response, error) {
-        response := http.Response{
-            StatusCode: mockStatusCode,
-            Body:       ioutil.NopCloser(bytes.NewReader([]byte(mockBody))),
-        }
-        return &response, mockErr
-    }
+	return func(req *http.Request) (*http.Response, error) {
+		response := http.Response{
+			StatusCode: mockStatusCode,
+			Body:       ioutil.NopCloser(bytes.NewReader([]byte(mockBody))),
+		}
+		return &response, mockErr
+	}
 }
 
 func TestLoadURL(t *testing.T) {
-    urlLoader := NewURLLoader(mockClient(200, "42", nil))
-    res, err := urlLoader("http://ignored")
-    require.NoError(t, err)
-    require.Equal(t, "42", string(res))
+	urlLoader := NewURLLoader(mockClient(200, "42", nil))
+	res, err := urlLoader("http://ignored")
+	require.NoError(t, err)
+	require.Equal(t, "42", string(res))
 }
 
 func TestLoadURLError(t *testing.T) {
-    urlLoader := NewURLLoader(mockClient(200, "42", fmt.Errorf("some error")))
-    res, err := urlLoader("http://ignored")
-    require.Error(t, err)
-    require.Nil(t, res)
+	urlLoader := NewURLLoader(mockClient(200, "42", fmt.Errorf("some error")))
+	res, err := urlLoader("http://ignored")
+	require.Error(t, err)
+	require.Nil(t, res)
 }
 
 func TestLoadURLErrorStatus(t *testing.T) {
-    urlLoader := NewURLLoader(mockClient(400, "42", nil))
-    res, err := urlLoader("http://ignored")
-    require.Error(t, err)
-    require.Nil(t, res)
+	urlLoader := NewURLLoader(mockClient(400, "42", nil))
+	res, err := urlLoader("http://ignored")
+	require.Error(t, err)
+	require.Nil(t, res)
 }
