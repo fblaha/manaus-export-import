71af01bd3637df52c5aeb5bfaf7b0f49be12a2ad
Filip Blaha <blahaf@gmail.com>
2020-11-12 23:36:35 +0100

    port fixes

--- backup.sh
+++ backup.sh
@@ -5,6 +5,6 @@ pushd  "$(dirname "$0")/backup" || exit
 # TODO make parametrized
 ls -t export*.zip | tail -n +4 | xargs -I {} rm {}
 
-~/go/bin/mns-export
+~/go/bin/mns-export --url=http://localhost:7777/
 
 popd || exit
